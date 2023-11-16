b0213891926a55b80d15d8696f84c22c21edd9e1
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 21:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+var value1 = parseInt(prompt("Enter First value"));
+var value2 = parseInt(prompt("Enter Second value"));
+var result = value1*2 + value2;
+alert("The final result is: " + result);
